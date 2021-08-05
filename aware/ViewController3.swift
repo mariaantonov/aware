725c0afd03bda38ed8ffc0b8c417d2cb67d75abd
Melly Mejia <mellymejia3@gmail.com>
2021-08-05 15:04:40 -0400

    down

--- aware/ViewController3.swift
+++ aware/ViewController3.swift
@@ -6,22 +6,115 @@
 //
 
 import UIKit
-import DropDown
+
+
 class ViewController3: UIViewController {
     
-    @IBOutlet weak var selectOption: UIButton!
-    let dropDown = DropDown()
-    let dropDownValues = ["walk", "run", "drinkwater" ]
+    @IBOutlet weak var plantImage: UIImageView!
+    
+    @IBOutlet weak var message: UILabel!
+    var pictures = ["stage1", "stage2", "stage3", "stage4"]
+    var location = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        plantImage.image = UIImage(named: pictures[0])
+        message.isHidden = true
+        
+    
 
         // Do any additional setup after loading the view.
-        dropDown.anchorView = selectOption
-        dropDown.dataSource = dropDownValues
+     
     }
     
+    @IBAction func walking(_ sender: Any) {
+       location += 1
+        if location > 3 {
+            plantImage.image = UIImage(named: pictures[3])
+            
+        }
+        else {
+            plantImage.image = UIImage(named: pictures[location])
+            
+        }
+        if location == 3
+        {
+            message.isHidden = false
+        }
+    }
+    
+    @IBAction func skinCare(_ sender: Any) {
+        location += 1
+        if location > 3 {
+            plantImage.image = UIImage(named: pictures[3])
+            
+        }
+        else {
+            plantImage.image = UIImage(named: pictures[location]) }
+        if location == 3
+        {
+            message.isHidden = false
+        }
+    }
+    
+    @IBAction func meditation(_ sender: Any) {
+        location += 1
+        if location > 3 {
+            plantImage.image = UIImage(named: pictures[3])
+            
+        }
+        else {
+            plantImage.image = UIImage(named: pictures[location]) }
+        if location == 3
+        {
+            message.isHidden = false
+        }
+    }
+    
+    @IBAction func meal(_ sender: Any) {
+        location += 1
+        if location > 3 {
+            plantImage.image = UIImage(named: pictures[3])
+            
+        }
+        else {
+            plantImage.image = UIImage(named: pictures[location]) }
+        if location == 3
+        {
+            message.isHidden = false
+        }
+    }
+    
+    @IBAction func cleaning(_ sender: Any) {
+        location += 1
+        if location > 3 {
+            plantImage.image = UIImage(named: pictures[3])
+            
+        }
+        else {
+            plantImage.image = UIImage(named: pictures[location]) }
+        if location == 3
+        {
+            message.isHidden = false
+        }
+    }
+    
+    @IBAction func water(_ sender: Any) {
+        location += 1
+        if location > 3 {
+            plantImage.image = UIImage(named: pictures[3])
+            
+        }
+        else {
+            plantImage.image = UIImage(named: pictures[location]) }
+        if location == 3
+        {
+            message.isHidden = false
+        }
+    }
+}
     
     
     
 
-}
+
