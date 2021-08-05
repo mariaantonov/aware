67241bfebdd369a9f14fcdb4a25929f684588223
Melly Mejia <mellymejia3@gmail.com>
2021-08-05 11:32:25 -0400

    dropdown

--- aware/ViewController3.swift
+++ aware/ViewController3.swift
@@ -6,24 +6,22 @@
 //
 
 import UIKit
-
+import DropDown
 class ViewController3: UIViewController {
-
+    
+    @IBOutlet weak var selectOption: UIButton!
+    let dropDown = DropDown()
+    let dropDownValues = ["walk", "run", "drinkwater" ]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        dropDown.anchorView = selectOption
+        dropDown.dataSource = dropDownValues
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
+    
 
 }
