f6a7be963af557c9ee388c90b347af147c89177c
Ogechi Okafor <oookafor410@gmail.com>
2021-08-06 10:42:34 -0400

    ok

---
+++ aware/CalenderCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalenderCell.swift
+//  weeklycalender
+//
+//  Created by Ogechi Okafor on 8/5/21.
+//
+
+import UIKit
+
+class CalenderCell: UICollectionViewCell{
+    
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
